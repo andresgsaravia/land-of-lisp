8144dfc5285549ac3b6b6024d264257ca220e558
Andrés G. Saravia <ags3006@member.fsf.org>
2012-02-27 17:59:27 -0600

    Finished Grand Theft Wumpus. Chapter 8.

--- wumpus.lisp
+++ wumpus.lisp
@@ -132,9 +132,69 @@
 (defun draw-city (fname)
   (ugraph->png fname *congestion-city-nodes* *congestion-city-edges*))
 
-(defun new-game (draw-city-file)
+(defun known-city-nodes ()
+  (mapcar (lambda (node)
+	    (if (member node *visited-nodes*)
+		(let ((n (assoc node *congestion-city-nodes*)))
+		  (if (eql node *player-pos*)
+		      (append n '(*))
+		      n))
+		(list node '?)))
+	  (remove-duplicates 
+	   (append *visited-nodes*
+		   (mapcan (lambda (node)
+			     (mapcar #'car
+				     (cdr (assoc node
+						 *congestion-city-edges*))))
+			   *visited-nodes*)))))
+
+(defun known-city-edges ()
+  (mapcar (lambda (node)
+	    (cons node (mapcar (lambda (x)
+				 (if (member (car x) *visited-nodes*)
+				     x
+				     (list (car x))))
+			       (cdr (assoc node *congestion-city-edges*)))))
+	  *visited-nodes*))
+
+(defun draw-known-city ()
+  (ugraph->png "~/city.dot" (known-city-nodes) (known-city-edges)))
+
+(defun new-game ()
   (setf *congestion-city-edges* (make-city-edges))
   (setf *congestion-city-nodes* (make-city-nodes *congestion-city-edges*))
   (setf *player-pos* (find-empty-node))
   (setf *visited-nodes* (list *player-pos*))
-  (draw-city draw-city-file))
+  (draw-known-city))
+
+(defun handle-new-place (edge pos charging)
+  (let* ((node (assoc pos *congestion-city-nodes*))
+	 (has-worm (and (member 'glow-worm node)
+			(not (member pos *visited-nodes*)))))
+    (pushnew pos *visited-nodes*)
+    (setf *player-pos* pos)
+    (draw-known-city)
+    (cond ((member 'cops edge) 
+	   (princ "You ran into the cops. Game Over."))
+	  ((member 'wumpus node)
+	   (if charging
+	       (princ "You found the Wumpus! You Win!")
+	       (princ "You ran into the Wumpus. Game Over")))
+	  (charging (princ "You wasted your last bullet. Game Over."))
+	  (has-worm (let ((new-pos (random-node)))
+		      (princ "You ran into the Glow Worm Gang! You're now at ")
+		      (princ new-pos)
+		      (handle-new-place nil new-pos nil))))))
+
+(defun handle-direction (pos charging)
+  (let ((edge (assoc pos
+		     (cdr (assoc *player-pos* *congestion-city-edges*)))))
+    (if edge
+	(handle-new-place edge pos charging)
+	(princ "That location does not exist!"))))
+
+(defun walk (pos) 
+  (handle-direction pos nil))
+
+(defun charge (pos)
+  (handle-direction pos t))
