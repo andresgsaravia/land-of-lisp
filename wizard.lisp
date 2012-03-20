cf9be454990faf61e012f33a6ee6dba1c621d0cc
Andres G. Saravia <ags3006@member.fsf.org>
2012-03-20 10:53:55 -0600

    Completed the wizard's adventure game. Chapter 17.

--- wizard.lisp
+++ wizard.lisp
@@ -27,6 +27,8 @@
 
 (defparameter *location* 'living-room)
 
+(defparameter *allowed-commands* '(look walk pickup inventory))
+
 (defun describe-location (location nodes)
   (cadr (assoc location nodes)))
 
@@ -70,9 +72,47 @@
 (defun inventory ()
   (cons 'items- (objects-at 'body *objects* *object-locations*)))
 
-;; Game REPL functions
+(defun have (object)
+  (member object (cdr (inventory))))
 
-(defparameter *allowed-commands* '(look walk pickup inventory))
+(defmacro game-action (command subj obj place &body body)
+  (let ((subject (gensym))
+	(object (gensym)))
+    `(progn (defun ,command (,subject ,object)
+	      (if (and (eq *location* ',place)
+		       (eq ,subject ',subj)
+		       (eq ,object ',obj)
+		       (have ',subj))
+		  ,@body
+		  '(i cannot ,command like that.)))
+	    (pushnew ',command *allowed-commands*))))
+
+(defparameter *chain-welded* nil)
+
+(game-action weld chain bucket attic
+  (if (and (have ' bucket)
+	   (not *chain-welded*))
+      (progn (setf *chain-welded* t)
+	     '(the chain is now securely welded to the bucket.))
+      '(you do not have a bucket)))
+
+(defparameter *bucket-filled* nil)
+
+(game-action dunk bucket well garden 
+  (if *chain-welded*
+      (progn (setf *bucket-filled* t)
+	     '(the bucket is now full of water.))
+      '(the water level is too low to reach.)))
+
+(game-action splash bucket wizard living-room 
+  (cond ((not *bucket-filled*) '(the bucket has nothing in it.))
+	((have 'frog) '(the wizard awakes and sees that you stole his frog.
+			he is so upset he banishes you to the
+			netherworlds- you lose! the end.))
+	(t '(the wizard awakens from his slumber and greets you warmly.
+	     he hands you the magic low-carb donut- you win! the end.))))
+
+;; Game REPL functions
 
 (defun game-read ()
   (let ((cmd (read-from-string
